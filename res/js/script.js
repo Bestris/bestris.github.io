dc5ca9e7a0104036a3c24f208f5300cb7812ff13
bestris <kennobestris@gmail.com>
2021-01-10 01:41:36 +0900

    Added digital clock. Brought back chromatic aberration effect.

--- res/js/script.js
+++ res/js/script.js
@@ -1,3 +1,39 @@
 $(window).on("load", function() {
+	var month_name = [
+		"JAN", "FEB", "MAR",
+		"APR", "MAY", "JUN",
+		"JUL", "AUG", "SEP",
+		"OCT", "NOV", "DEC"
+	];
+
+	function showTime() {
+		var d = new Date();
+		var hh = d.getHours();
+		var mm = d.getMinutes();
+		var date = d.getDate();
+		var month = d.getMonth();
+		var year = d.getFullYear();
+		var meridiem = "AM";
+
+		if(hh == 0) {
+			hh = 12;
+		}
+		if(hh > 12) {
+			hh -= 12;
+			meridiem = "PM";
+		}
+
+		hh = (hh < 10) ? "0" + hh : hh;
+		mm = (mm < 10) ? "0" + mm : mm;
+		month = month_name[month];
+
+		time = meridiem + " " + hh + ":" + mm + "<br />" + month + ". " + date + " " + year;
+		$("#clock").html(time);
+
+		setTimeout(showTime, 1000);
+	}
+
+	showTime();
+
 	$("#load_screen").addClass("loadscreen_hidden");
 });
